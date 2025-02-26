dfd476f3fc72270e22b8d10f5d6aacf0a201e394
Energia24-7 <fastindustrialsupply@gmail.com>
2025-02-26 16:38:22 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function abrirCalculoUbicacion() {
+    window.location.href = "https://energia24-7.github.io/calc1/";
+}
+
+function abrirDimensionSistema() {
+    window.location.href = "dimension_sistema.html";
+}
